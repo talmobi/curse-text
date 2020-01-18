af7df0403d6563b63afa436c7e4990f99cc38f6e
Christian Talmo <talmo.christian@gmail.com>
2020-01-18 17:05:34 +0200

    feat: default intensity to 0.5

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const zalgo = require( 'zalgo-js' ).default
 
 module.exports = function curseText ( text, opts ) {
+  opts = opts || {}
+  if ( opts.intensity == null ) {
+    opts.intensity = 0.5
+  }
+
   return zalgo( text, opts )
 }
