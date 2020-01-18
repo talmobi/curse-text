cfe0e6d9fb32105caefcfc9021cd934e31d79a64
Christian Talmo <talmo.christian@gmail.com>
2020-01-18 16:11:58 +0200

    feat: handle arg0 to set intensity

--- cli.js
+++ cli.js
@@ -2,6 +2,26 @@
 
 const curse = require( './main.js' )
 
+const args = process.argv.slice( 2 )
+
+let _intensity = 0.5
+
+if ( args[ 0 ] ) {
+  const n = Number( args[ 0 ] )
+
+  if ( typeof n !== 'number' ) {
+    console.log( 'argument has to be a decimal number between 0 and 1' )
+    process.exit( 1 )
+  }
+
+  if ( Number.isNaN( n ) ) {
+    console.log( 'argument has to be a decimal number between 0 and 1' )
+    process.exit( 1 )
+  }
+
+  _intensity = n
+}
+
 let buffer = ''
 process.stdin.on( 'data', function ( chunk ) {
   buffer += chunk
